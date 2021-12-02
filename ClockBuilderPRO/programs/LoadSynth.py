ebb74d52885e18d9e2ed5bdde1f79c1b4517b84f
Charles R. Strohman <crs5@lnx750.classe.cornell.edu>
2021-12-02 10:43:47 -0500

    cleaned up LoadSynth.py

--- ClockBuilderPRO/programs/LoadSynth.py
+++ ClockBuilderPRO/programs/LoadSynth.py
@@ -30,31 +30,31 @@ if args.synth_id == "r0a" :
     i2c_port = "2"
     i2c_addr = "0x77"
     i2c_mux_mask = "0x01"
-    Def_List = dir+"/Si5341-RevD-default-Registers.h"
+    Def_List = dir+"/../defaults/Si5341-RevD-default-Registers.h"
 elif args.synth_id == "r0b" :
     # SI5395 on mux channel 1 (mask = 0x02)
     i2c_port = "2"
     i2c_addr = "0x6b"
     i2c_mux_mask = "0x02"
-    Def_List = dir+"/Si5395-RevA-default-Registers.h"
+    Def_List = dir+"/../defaults/Si5395-RevA-default-Registers.h"
 elif args.synth_id == "r1a" :
     # SI5395 on mux channel 2 (mask = 0x04)
     i2c_port = "2"
     i2c_addr = "0x6b"
     i2c_mux_mask = "0x04"
-    Def_List = dir+"/Si5395-RevA-default-Registers.h"
+    Def_List = dir+"/../defaults/Si5395-RevA-default-Registers.h"
 elif args.synth_id == "r1b" :
     # SI5395 on mux channel 3 (mask = 0x08)
     i2c_port = "2"
     i2c_addr = "0x6b"
     i2c_mux_mask = "0x08"
-    Def_List = dir+"/Si5395-RevA-default-Registers.h"
+    Def_List = dir+"/../defaults/Si5395-RevA-default-Registers.h"
 elif args.synth_id == "r1c" :
     # SI5395 on mux channel 4 (mask = 0x10)
     i2c_port = "2"
     i2c_addr = "0x6b"
     i2c_mux_mask = "0x10"
-    Def_List = dir+"/Si5395-RevA-default-Registers.h"
+    Def_List = dir+"/../defaults/Si5395-RevA-default-Registers.h"
 
 serPort = "/dev/"+args.tty
 
@@ -66,9 +66,11 @@ def get_command(command):
     # just ensure command has newline 
     command = command.rstrip()
     command = command + '\r\n'
+    # show what will be sent to the board    
     print(command)
     ser.write(command.encode()) # write one char from an int
     done = False
+    # wait for the MCU to send back a "%" prompt    
     while ( not done ):
         line  = ser.readline().rstrip()
         if ( len(line) and chr(line[0]) == '%' ) :
@@ -77,9 +79,9 @@ def get_command(command):
             lines.append(line.decode())
     return lines
 
-#write to ClockSynthesizer at 0x6b
-#When Read print out output of get_command
-def write_reg(ListOfRegs,ListOfDefs,Optimize,Read):
+#write to ClockSynthesizer
+#When 'noisy' print out returned data of get_command
+def write_reg(ListOfRegs,ListOfDefs,Optimize,Noisy):
     HighByte = -1
     Match = False
     for register in ListOfRegs:
@@ -110,43 +112,47 @@ def write_reg(ListOfRegs,ListOfDefs,Optimize,Read):
             if ChangePage:
                 # write the new page number to address 0x01
                 command = "i2cwr "+i2c_port+" "+i2c_addr+" 0x01 1 "+register[0][0:4]+""
-                if Read:
+                if Noisy:
                     print(get_command(command))
                 else:
                     get_command(command)
             # write the register address and value
             command = "i2cwr "+i2c_port+" "+i2c_addr+" 0x"+register[0][4:6]+" 1 "+register[1]+""
-            if Read:
+            if Noisy:
                 print(get_command(command))
             else:
                 get_command(command)
 
     #Set page back to 0 in the end
-    if Read:
+    if Noisy:
         print(get_command("i2cwr "+i2c_port+" "+i2c_addr+" 0x01 1 0"))
     else:
         get_command("i2cwr "+i2c_port+" "+i2c_addr+" 0x01 1 0")
         
+# send 'help' command and print out results to show that the MCU is communicating
 print(get_command("help"))
-#enable ports 6 and 7 on U84 at 0x70
+
+# eventually move the I2C register port setup elsewhere.
+# enable ports 6 and 7 on U84 at 0x70
 print(get_command("i2cw "+i2c_port+" 0x70 1 0xc0"))
-#Ping the registers at 0x20 and 0x21 to make sure they are indeed enabled
+# Ping the registers at 0x20 and 0x21 to make sure they are indeed enabled
 print(get_command("i2crr "+i2c_port+" 0x20 0x06 1"))
 print(get_command("i2crr "+i2c_port+" 0x21 0x06 1"))
-#Setting Control Registers at 0x20 on U88 (TCA9555) to have outputs on P07, P03..P00, P15..P12, and P10 (set '0' for outputs)
+# Setting Control Registers at 0x20 on U88 (TCA9555) to have outputs on P07, P03..P00, P15..P12, and P10 (set '0' for outputs)
 print(get_command("i2cwr "+i2c_port+" 0x20 0x06 1 0x70")) # 0b01110000
 print(get_command("i2cwr "+i2c_port+" 0x20 0x07 1 0xc2")) # 0b11000010
-#Setting U88 outputs on P07 and P10 to '1' to negate active-lo reset signals. All others to '0'
+# Setting U88 outputs on P07 and P10 to '1' to negate active-lo reset signals. All others to '0'
 print(get_command("i2cwr "+i2c_port+" 0x20 0x02 1 80")) #0b10000000
 print(get_command("i2cwr "+i2c_port+" 0x20 0x03 1 01")) #0b00000001
 
-# disable I2C routes to register chips, enable route to synth
+# enable only the route through the I2C mux to the selected synth
 print(get_command("i2cw "+i2c_port+" 0x70 1 "+i2c_mux_mask+""))
 # ping the synth
 print(get_command("i2cr "+i2c_port+" "+i2c_addr+"  1"))
 
 
-pdb.set_trace()
+#pdb.set_trace()
+# open files and create/initialize variables
 regfile=open(args.Reg_List, 'r', encoding='iso-8859-1')
 deffile=open(Def_List, 'r')
 PreambleList = []
@@ -165,11 +171,11 @@ while True:
         break    
     # set or clear section flags at the start or end of the register section
     if (line.find("Start configuration registers") > 0):
-        print(line)
+        print("Starting to read default register settings")
         InRegisters = 1
         continue
     elif (line.find("End configuration registers") > 0):
-        print(line)
+        print("Finished reading default register settings")
         InRegisters = 0
         # bail out at the end of the register section
         break
@@ -179,7 +185,7 @@ while True:
         if InRegisters :
             # line is from the register sections
             if line.find("/*") < 0 :
-                # not a comment line
+                # this is not a comment line that is embedded in a data section
                 for words in line.split('"'):
                     # extract the register address and value
                     reg_loc = words.find("0x")
@@ -196,27 +202,27 @@ while True:
         break    
     # set or clear section flags at the start or end of each section
     if (line.find("Start configuration preamble") > 0):
-        print(line)
+        print("Starting to read the configuration preamble")
         InPreamble = 1
         continue
     elif (line.find("End configuration preamble") > 0):
-        print(line)
+        print("Finished reading the configuration preamble")
         InPreamble = 0
         continue
     elif (line.find("Start configuration registers") > 0):
-        print(line)
+        print("Starting to read the configuration data")
         InRegisters = 1
         continue
     elif (line.find("End configuration registers") > 0):
-        print(line)
+        print("Finished reading the configuration data")
         InRegisters = 0
         continue
     elif (line.find("Start configuration postamble") > 0):
-        print(line)
+        print("Starting to read the configuration postamble")
         InPostamble = 1
         continue
     elif (line.find("End configuration postamble") > 0):
-        print(line)
+        print("Finished reading the configuration postamble")
         InPostamble = 0
         # bail out at the end of the postamble
         break
@@ -224,35 +230,35 @@ while True:
     # line does not contain a section marker
     else:
         if InPreamble or InRegisters or InPostamble :
-            if (line.find("0x0858") > 0) :
-               print(line)
-               pdb.set_trace()
             # line is from one of the register sections
             if line.find("/*") < 0 :
                 # not a comment line that is embedded with a section
                 for words in line.split('"'):
-                    if line.find("/*") < 0 :
-                        # not a comment line
-                        # extract the register address and value
-                        reg_loc = words.find("0x")
-                        reg = words[reg_loc:reg_loc+6]
-                        val_loc = words.find("0x",reg_loc + 1)
-                        val = words[val_loc:val_loc+4]
-                        # append the register address and value to the appropriate list
-                        if InPreamble:
-                            PreambleList.append((reg,val))
-                        elif InRegisters:
-                            RegisterList.append((reg,val))   
-                        elif InPostamble:
-                            PostambleList.append((reg,val))
+                    # not a comment line
+                    # extract the register address and value
+                    reg_loc = words.find("0x")
+                    reg = words[reg_loc:reg_loc+6]
+                    val_loc = words.find("0x",reg_loc + 1)
+                    val = words[val_loc:val_loc+4]
+                    # append the register address and value to the appropriate list
+                    if InPreamble:
+                        PreambleList.append((reg,val))
+                    elif InRegisters:
+                        RegisterList.append((reg,val))   
+                    elif InPostamble:
+                        PostambleList.append((reg,val))
 
+# send the preamble, data, and postamble to the synth
 def LoadClock(PreList,RegList,PostList,DefList,Read):
+    # for the preamble, send all data    
     Optimize = False
     write_reg(PreList,DefList,Optimize,Read)
     time.sleep(1) #only need 300 msec
+    # for the data section, eliminate data that matches the default configuration    
     Optimize = True
     write_reg(RegList,DefList,Optimize,Read)
     time.sleep(1)
+    # for the postamble, send all data
     Optimize = False
     write_reg(PostList,DefList,Optimize,Read)
 
@@ -262,5 +268,5 @@ LoadClock(PreambleList, RegisterList, PostambleList, DefaultList, not args.quiet
 # disable all of the channels in the switch
 print(get_command("i2cw 2 0x70 1 0x00"))
 
-#if ser.is_open:
+# close the tty port:
 ser.close()
